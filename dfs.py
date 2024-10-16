7a62fd3068e6dba1e9f45df3df16fdb439b3cb3e
LuisSalazar02 <luissalazar2002@hotmail.com>
2024-10-16 00:03:49 -0600

    Initial commit

---
+++ dfs.py
@@ -0,0 +1,5 @@
+"""
+@description: DFS algortithm implementation
+@authors: Aracelli Melissa Boza Zabarburú A01662934, Adolfo Hernández Fernández A01664412, Luis Enrique Salazar Pérez A00833460
+@date: 2024/17/10 
+"""
